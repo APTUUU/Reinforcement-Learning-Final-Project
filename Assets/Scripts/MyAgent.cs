d693954334b3cab9ed8c563d8c19475a4605c7be
yassine jemni <yassinejemni6@gmail.com>
2023-11-22 19:47:13 +0100

    add code for agent movement

--- Assets/Scripts/MyAgent.cs
+++ Assets/Scripts/MyAgent.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using Unity.MLAgents;
@@ -10,14 +11,14 @@ public class MyAgent : Agent
     Rigidbody m_rigidbody;
     float m_speed = 20;
 
-    public GameObject Spawner;
+    public GameObject Spawner; // Reference to the GameObject that acts as the spawner
 
-    private Vector3 startingPosition = new Vector3(162.3f, 0.0f, -1407.01f);
+    private Vector3 startingPosition = new Vector3(162.3f, 0.0f, -1407.01f); // Starting position of the agent
 
-    private float boundXLeft = -20f;
-    private float boundXRight = 20f;
+    private float boundXLeft = -20f; // Left boundary for the agent's movement
+    private float boundXRight = 20f; // Right boundary for the agent's movement
 
-    private enum ACTIONS
+    private enum ACTIONS // Actions that the agent can take
     {
         LEFT = 0,
         NOTHING = 1,
@@ -26,40 +27,36 @@ public class MyAgent : Agent
 
     void Start()
     {
-        m_rigidbody = GetComponent<Rigidbody>();
+        m_rigidbody = GetComponent<Rigidbody>(); // Get the Rigidbody component of this GameObject
     }
 
     public override void OnEpisodeBegin()
     {
-        // We reset the agent's position
+        // We reset the agent's position to the starting position at the beginning of each episode
         transform.localPosition = startingPosition;
     }
-
-    public override void CollectObservations(VectorSensor sensor)
-    {
-        // We don't need this function now because we use the RayPerceptionSensor
-        // Note however that we could add additional observations here, if we wanted to, like the speed & velocity of the agent etc.
-    }
-
+    
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+        // Heuristic method to enable manual control of the agent for testing purposes
+
         ActionSegment<int> actions = actionsOut.DiscreteActions;
 
-        var horizontal = Input.GetAxisRaw("Horizontal");
-        var vertical = Input.GetAxisRaw("Vertical");
+        var horizontal = Input.GetAxisRaw("Horizontal"); // Get input from the Horizontal axis (e.g., arrow keys)
+        var vertical = Input.GetAxisRaw("Vertical"); // Get input from the Vertical axis
 
         if (horizontal == -1)
         {
-            actions[0] = (int)ACTIONS.LEFT;
+            actions[0] = (int)ACTIONS.LEFT; // Set action to move left if input is -1 (left arrow)
         }
         else if (horizontal == +1)
         {
-            actions[0] = (int)ACTIONS.RIGHT;
+            actions[0] = (int)ACTIONS.RIGHT; // Set action to move right if input is +1 (right arrow)
         }
         else
         {
-            actions[0] = (int)ACTIONS.NOTHING;
+            actions[0] = (int)ACTIONS.NOTHING; // Set action to do nothing if no horizontal input
         }
 
     }
@@ -67,35 +64,37 @@ public class MyAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        var actionTaken = actions.DiscreteActions[0];
+        // Actions executed by the agent based on the received action
+
+        var actionTaken = actions.DiscreteActions[0]; // Get the action taken
 
         switch (actionTaken)
         {
             case (int)ACTIONS.NOTHING:
-                break;
+                break; // Do nothing
             case (int)ACTIONS.LEFT:
-                // We translate the agent's body to the left if it can move left
+                // Move the agent's body to the left if it can move left within the defined boundary
                 if (transform.localPosition.x > boundXLeft)
                     transform.Translate(-Vector3.right * m_speed * Time.deltaTime);
                 else
                 {
-                    AddReward(-1);
-                    EndEpisode();
+                    AddReward(-1); // Decrease reward for hitting the boundary
+                    EndEpisode(); // End the episode
                 }
                 break;
             case (int)ACTIONS.RIGHT:
-                // We translate the agent's body to the right if it can move right
+                // Move the agent's body to the right if it can move right within the defined boundary
                 if (transform.localPosition.x > boundXRight)
                     transform.Translate(Vector3.right * m_speed * Time.deltaTime);
                 else
                 {
-                    AddReward(-1);
-                    EndEpisode();
+                    AddReward(-1); // Decrease reward for hitting the boundary
+                    EndEpisode(); // End the episode
                 }
                 break;
         }
 
-        AddReward(0.1f);
+        AddReward(0.1f); // Add a small positive reward after each action
     }
 
     private void OnTriggerEnter(Collider other)
@@ -103,7 +102,7 @@ public class MyAgent : Agent
         // If the agent collided with a bear, we delete the bears & end the episode
         if (other.tag == "Bear")
         {
-            // We delete each Ball object that we have spawned so far 
+            // Delete each bear object spawned by the spawner
             var parent = Spawner.transform;
             int numberOfChildren = parent.childCount;
 
@@ -115,7 +114,7 @@ public class MyAgent : Agent
                 }
             }
 
-            EndEpisode();
+            EndEpisode(); // End the episode
         }
     }
 
